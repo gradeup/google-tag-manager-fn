b4a2755d582ac9625d1ad7eef46755a387ececd8
codehimanshu <himanshuagrawal1998@gmail.com>
2020-05-12 18:40:35 +0530

    add nonce in gtm noscript

--- src/helpers/append-gtm.js
+++ src/helpers/append-gtm.js
@@ -13,9 +13,16 @@ const appendGtm = ({id, preview = '', auth = '', events = {}, dataLayerName = 'd
 	}
 
 	/* Google Tag Manager (script) : Goes in the head tag */
-	const noscript = `
+	let noscript;
+	if(nonce === '') {
+		noscript = `
 		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js', ${eventsVar} });var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl+'${authVar}${previewVar}';f.parentNode.insertBefore(j,f); })(window,document, 'script', '${dataLayerName}', '${id}');
 		`;
+	} else {
+		noscript = `
+		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js', ${eventsVar} });var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.nonce=${nonce};j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl+'${authVar}${previewVar}';f.parentNode.insertBefore(j,f); })(window,document, 'script', '${dataLayerName}', '${id}');
+		`;
+	}
 	appendToHTML({data: noscript, head: true, reactFlag, nonce});
 	/* Google Tag Manager (script) : Goes in the head tag */
 
