2ebc1211e27df418d300d8b1128d22756fda7062
codehimanshu <himanshuagrawal1998@gmail.com>
2020-05-25 16:45:54 +0530

    fix nonce

--- src/helpers/append-gtm.js
+++ src/helpers/append-gtm.js
@@ -20,7 +20,7 @@ const appendGtm = ({id, preview = '', auth = '', events = {}, dataLayerName = 'd
 		`;
 	} else {
 		noscript = `
-		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js', ${eventsVar} });var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.nonce=${nonce};j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl+'${authVar}${previewVar}';f.parentNode.insertBefore(j,f); })(window,document, 'script', '${dataLayerName}', '${id}');
+		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js', ${eventsVar} });var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.nonce='${nonce}';j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl+'${authVar}${previewVar}';f.parentNode.insertBefore(j,f); })(window,document, 'script', '${dataLayerName}', '${id}');
 		`;
 	}
 	appendToHTML({data: noscript, head: true, reactFlag, nonce});
